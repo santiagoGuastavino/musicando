f4cb69b85f4c0ccdf28066eaffe62e29b8f3be08
santiagoGuastavino <san.guasta@gmail.com>
2021-09-23 11:41:26 -0300

    trying deploy

--- src/controllers/songsController.js
+++ src/controllers/songsController.js
@@ -2,51 +2,57 @@ let db = require('../database/models');
 
 let songsController = {
     list: async (req, res) => {
-        let warning = null;
-        let allSongs = await db.Cancion.findAll();
-        try {
-            let limit = 50;
-            let offset = 0;
-            let fiftySongs = await db.Cancion.findAll({
-                include: ['album'],
-                limit: limit,
-                offset: offset
-            }, {
-                include: [
-                    'album',
-                    'genero'
-                ]
-            });
-            try {
-                fiftySongs = fiftySongs.map(song => {
-                    console.log(song);
-                    song = {
-                        nombre: song.nombre,
-                        album: song.album.dataValues.titulo,
-                        compositor: song.compositor,
-                        genero: song.genero.dataValues.genero.nombre,
+        db.Cancion.findAll({
+            // attributes: 
+            limit: 50
+        })
+            .then(canciones => res.json(canciones))
+            .catch(err => res.json(err))
+        // let warning = null;
+        // let allSongs = await db.Cancion.findAll();
+        // try {
+        //     let limit = 50;
+        //     let offset = 0;
+        //     let fiftySongs = await db.Cancion.findAll({
+        //         include: ['album'],
+        //         limit: limit,
+        //         offset: offset
+        //     }, {
+        //         include: [
+        //             'album',
+        //             'genero'
+        //         ]
+        //     });
+        //     try {
+        //         fiftySongs = fiftySongs.map(song => {
+        //             console.log(song);
+        //             song = {
+        //                 nombre: song.nombre,
+        //                 album: song.album.dataValues.titulo,
+        //                 compositor: song.compositor,
+        //                 genero: song.genero.dataValues.genero.nombre,
 
-                    }
-                    return song
-                })
-                res.status(200).json({
-                    status: 200,
-                    total: allSongs.length,
-                    resultsPerPage: 50,
-                    canciones: fiftySongs
-                })
-            } catch(err) {
-                res.status(500).json({
-                    status: 500,
-                    err
-                });
-            };
-        } catch(err) {
-            res.status(500).json({
-                status: 500,
-                err
-            });
-        };
+        //             }
+        //             return song
+        //         })
+        //         res.status(200).json({
+        //             status: 200,
+        //             total: allSongs.length,
+        //             resultsPerPage: 50,
+        //             canciones: fiftySongs
+        //         })
+        //     } catch(err) {
+        //         res.status(500).json({
+        //             status: 500,
+        //             err
+        //         });
+        //     };
+        // } catch(err) {
+        //     res.status(500).json({
+        //         status: 500,
+        //         err
+        //     });
+        // };
         // let limit = 50;
         // let offset = 0;
         // if (req.params.id) {
