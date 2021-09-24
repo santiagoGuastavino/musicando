01f20f99b1ee089ac92b96ab293d24daf39bf6b6
santiagoGuastavino <san.guasta@gmail.com>
2021-09-24 18:13:28 -0300

    config production database

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "b758f2c16ede1f",
+    "password": "cdc88a90",
+    "database": "heroku_0ad497021f3955e",
+    "host": "us-cdbr-east-04.cleardb.com",
     "dialect": "mysql"
   }
 }
